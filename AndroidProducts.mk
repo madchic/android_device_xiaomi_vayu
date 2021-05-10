8351b30cdbc8784f539cf8c7d7211d90a3b1fc05
Fran P <sweetydesigns@gmail.com>
2021-05-10 20:49:39 +0200

    vayu: Add lineage product

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     arrow_vayu-eng \
     arrow_vayu-userdebug \
     arrow_vayu-user
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_vayu.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_vayu-eng \
+    lineage_vayu-userdebug \
+    lineage_vayu-user
---
+++ lineage_vayu.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2018-2021 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+# Inherit common LineageOS configurations
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+PRODUCT_NAME := lineage_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := Poco X3 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="vayu"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+DEVICE_MAINTAINER := PacMM79
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
