41841785a4c55abe4cf3b4170b2359f20ab86cfa
Fran P <sweetydesigns@gmail.com>
2021-07-04 20:48:58 +0200

    Revert "vayu: WIP Switch to stats service mock"
    This reverts commit 512c12da9c273f675b5041b343515faf60c8f1c6.

--- device.mk
+++ device.mk
@@ -171,7 +171,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.vayu \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service
 
 # Sensors
 PRODUCT_PACKAGES += \
